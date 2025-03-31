33a4c202b97a0135fc6d5081bc15c907ed1ed7f4
Aurelia Hudak <aurelia@Aurelias-MacBook-Pro.local>
2025-03-31 13:13:51 -0400

    Add header and footer

--- js/script.js
+++ js/script.js
@@ -34,3 +34,12 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Error loading sidebar:', error));
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".hamburger-menu");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuButton.addEventListener("click", function () {
+        navLinks.classList.toggle("show");
+    });
+});
